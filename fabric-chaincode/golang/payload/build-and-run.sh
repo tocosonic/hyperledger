42ce6dc268724f641858f7d5e2a7d5c8d5cbd3e2
Michael Hinkel <michael.hinkel@deutschebahn.com>
2018-03-15 12:58:57 +0100

    updated build and run script to not need the go filename

--- fabric-chaincode/golang/payload/build-and-run.sh
+++ fabric-chaincode/golang/payload/build-and-run.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-# param 1: source file = chaincode-id-path
-
-if [ -z $1 ] ; then
-  echo MISSING PARAMETER
-  echo "Parameter 1: Please pass the filename of the chaincode source file (e.g. chaincode_example02.go)"
-  exit -1
-fi
-
-go build -o /chaincode/bin/chaincode /chaincode/exchange/$CONTAINER_ID/$1 
+go build -o /chaincode/bin/chaincode ./exchange/$CONTAINER_ID
 /chaincode/bin/chaincode -peer.address=$CORE_PEER_CHAINCODELISTENADDRESS
