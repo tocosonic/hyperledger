047e9d0e45a0303d0a252a1946a6bedac508d598
Michael Hinkel <michael.hinkel-extern@deutschebahn.com>
2018-07-18 14:58:52 +0200

    update to build-and-run file to use java -jar command to start cc

--- fabric-chaincode/java/payload/build-and-run.sh
+++ fabric-chaincode/java/payload/build-and-run.sh
@@ -5,4 +5,5 @@ cp -rf root/ application
 cp -rf exchange/$CONTAINER_ID/* application/
 cd application
 gradle build
-gradle run -PappArgs="['-a=$CORE_PEER_CHAINCODELISTENADDRESS']"
+#gradle run -PappArgs="['-a=$CORE_PEER_CHAINCODELISTENADDRESS']"
+java -jar build/chaincode.jar --peerAddress $CORE_PEER_CHAINCODELISTENADDRESS
