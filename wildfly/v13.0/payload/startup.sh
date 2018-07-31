08da3ac43940958ab01c4630ddbb8d412ab3d1c0
Michael Hinkel <michael.hinkel-extern@deutschebahn.com>
2018-07-31 15:34:32 +0200

    fixed copy operation

--- wildfly/v13.0/payload/startup.sh
+++ wildfly/v13.0/payload/startup.sh
@@ -6,7 +6,7 @@ CFG_DIR=${WILDFLY_BASE_DIR}/standalone/configuration
 #copy default default config if files don't exist
 if [ -z "$(ls -A $CFG_DIR)"  ]; then
   echo "copy default configuration data"
-  cp -rf ${WILDFLY_BASE_DIR}/standalone.bak/configuration ${CFG_DIR}/
+  cp -rf ${WILDFLY_BASE_DIR}/standalone.bak/configuration/* ${CFG_DIR}/
 fi
 
 echo "start-up Wildfly..."
