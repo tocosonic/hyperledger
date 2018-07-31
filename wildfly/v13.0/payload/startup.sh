430668dff42d66c973881a0724e6d5e3500fdaed
Michael Hinkel <michael.hinkel-extern@deutschebahn.com>
2018-07-31 14:59:00 +0200

    fix

--- wildfly/v13.0/payload/startup.sh
+++ wildfly/v13.0/payload/startup.sh
@@ -2,17 +2,12 @@
 
 WILDFLY_BASE_DIR=/opt/jboss/wildfly
 CFG_DIR=${WILDFLY_BASE_DIR}/standalone/configuration
-DATA_DIR=${WILDFLY_BASE_DIR}/standalone/data
 
-#copy default data and default config if files don't exist
+#copy default default config if files don't exist
 if [ -z "$(ls -A $CFG_DIR)"  ]; then
   echo "copy default configuration data"
-  cp -rf ${WILDFLY_BASE_DIR}/standalone.bak/configuiration ${CFG_DIR}/
-fi
-if [ -z "$(ls -A $DATA_DIR)" ]; then
-  echo "copy default data"
-  cp -rf ${WILDFLY_BASE_DIR}/standalone.bak/data ${DATA_DIR}/
+  cp -rf ${WILDFLY_BASE_DIR}/standalone.bak/configuration ${CFG_DIR}/
 fi
 
 echo "start-up Wildfly..."
-/opt/jboss/wildfly/bin/standalone "$@"
+/opt/jboss/wildfly/bin/standalone.sh "$@"
