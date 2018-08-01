e7d41725049912c64a97c2ba37ca1b57a74eb1cb
Michael Hinkel <michael.hinkel-extern@deutschebahn.com>
2018-08-01 10:41:00 +0200

    new Dockerfile for CouchDB, which backs-up the config data and copies the config data back upon its first start...

---
+++ couchdb/standalone/payload/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+COUCHDB_BASE_DIR=/opt/couchdb
+CFG_DIR=${COUCHDB_BASE_DIR}/etc
+
+#copy default default config if files don't exist
+if [ -z "$(ls -A $CFG_DIR)"  ]; then
+  echo "copy default configuration data"
+  cp -rf ${COUCHDB_BASE_DIR}/etc.bak/* ${CFG_DIR}/
+fi
+
+echo "start-up CouchDB..."
+/docker-entrypoint.sh "$@"
