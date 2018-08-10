c8870910870ce48c2e1285a871ecc65db53d013b
Michael Hinkel <michael.hinkel-extern@deutschebahn.com>
2018-08-10 17:39:38 +0200

    changed copy to not overwrite existing files

--- wildfly/v13.0/payload/startup.sh
+++ wildfly/v13.0/payload/startup.sh
@@ -4,10 +4,8 @@ WILDFLY_BASE_DIR=/opt/jboss/wildfly
 CFG_DIR=${WILDFLY_BASE_DIR}/standalone/configuration
 
 #copy default default config if files don't exist
-if [ -z "$(ls -A $CFG_DIR)"  ]; then
-  echo "copy default configuration data"
-  cp -rf ${WILDFLY_BASE_DIR}/standalone.bak/configuration/* ${CFG_DIR}/
-fi
+echo "copy default configuration data"
+cp -nr ${WILDFLY_BASE_DIR}/standalone.bak/configuration/* ${CFG_DIR}/
 
 echo "start-up Wildfly..."
 /opt/jboss/wildfly/bin/standalone.sh "$@"
