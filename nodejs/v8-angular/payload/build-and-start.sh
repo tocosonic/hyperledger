5e1eb6a11bafcff9a881f43ed806bd98ea319352
tocosonic <34197093+tocosonic@users.noreply.github.com>
2019-02-25 13:18:07 +0100

    Update build-and-start.sh
    commented-out storybook-lines

--- nodejs/v8-angular/payload/build-and-start.sh
+++ nodejs/v8-angular/payload/build-and-start.sh
@@ -4,9 +4,9 @@ export HOME=/nodejs
 cd $HOME
 
 npm install -g @angular/cli
-npm install -g @storybook/cli
+#npm install -g @storybook/cli
 npm install
 npm run build
-npm run build-storybook
+#npm run build-storybook
 
 npm start
