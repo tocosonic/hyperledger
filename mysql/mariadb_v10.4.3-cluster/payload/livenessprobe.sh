76f0afb14d62686c1b452c376dc0e3ede5c527dd
Michael Hinkel <michael.hinkel-extern@deutschebahn.com>
2019-03-11 00:04:58 +0100

    fixed probes

--- mysql/mariadb_v10.4.3-cluster/payload/livenessprobe.sh
+++ mysql/mariadb_v10.4.3-cluster/payload/livenessprobe.sh
@@ -3,7 +3,7 @@
 OUT=/status/$HOSTNAME-live.log
 touch $OUT
 
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD status 1>> $OUT 2>> $OUT
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD ping 1>> $OUT 2>> $OUT
 
 if [ $? -ne 0 ]; then
   echo "status not successful" >> $OUT
--- mysql/mariadb_v10.4.3-cluster/payload/readinessprobe.sh
+++ mysql/mariadb_v10.4.3-cluster/payload/readinessprobe.sh
@@ -4,7 +4,7 @@ OUT=/status/$HOSTNAME-ready.log
 touch $OUT
 
 if [ -f /status/clusterup ]; then
-  mysql -h $HOSTNAME.${APPLICATION_NAME}-ss.$PROJECT_NAMESPACE.svc -u root -p$MYSQL_ROOT_PASSWORD -e "select 1" 1>> $OUT 2>> $OUT
+  mysql -h "127.0.0.1" -u root -p$MYSQL_ROOT_PASSWORD -e "select 1" 1>> $OUT 2>> $OUT
   
   if [ $? -ne 0 ]; then
     echo "select not ok" >> $OUT
