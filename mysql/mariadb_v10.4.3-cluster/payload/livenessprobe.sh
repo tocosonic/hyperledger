417abe737ee787e1a7d48b239f1db76056bc1af4
Michael Hinkel <michael.hinkel-extern@deutschebahn.com>
2019-03-11 12:00:32 +0100

    removed log outputs of probes

--- mysql/mariadb_v10.4.3-cluster/payload/livenessprobe.sh
+++ mysql/mariadb_v10.4.3-cluster/payload/livenessprobe.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-OUT=/status/$HOSTNAME-live.log
-touch $OUT
+#OUT=/status/$HOSTNAME-live.log
+#touch $OUT
 
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD ping 1>> $OUT 2>> $OUT
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD ping
+# 1>> $OUT 2>> $OUT
 
 if [ $? -ne 0 ]; then
-  echo "status not successful" >> $OUT
+#  echo "status not successful" >> $OUT
   exit -1
-else
-  echo "ok" >> $OUT
-  exit 0
 fi
+
+exit 0
--- mysql/mariadb_v10.4.3-cluster/payload/readinessprobe.sh
+++ mysql/mariadb_v10.4.3-cluster/payload/readinessprobe.sh
@@ -1,35 +1,26 @@
 #!/bin/bash
 
-OUT=/status/$HOSTNAME-ready.log
-touch $OUT
+#OUT=/status/$HOSTNAME-ready.log
+#touch $OUT
 
-#if [ -f /status/clusterup ]; then
-  mysql -h "127.0.0.1" -u root -p$MYSQL_ROOT_PASSWORD -e "select 1" 1>> $OUT 2>> $OUT
+  mysql -h "127.0.0.1" -u root -p$MYSQL_ROOT_PASSWORD -e "select 1"
+  # 1>> $OUT 2>> $OUT
   
   if [ $? -ne 0 ]; then
-    echo "select not ok, trying root without password" >> $OUT
+#    echo "select not ok, trying root without password" >> $OUT
     
-    mysql -h "127.0.0.1" -u root -e "select 1" 1>> $OUT 2>> $OUT
+    mysql -h "127.0.0.1" -u root -e "select 1"
+    # 1>> $OUT 2>> $OUT
     if [ $? -ne 0 ]; then
-      echo "select not ok, trying without user" >> $OUT
+#      echo "select not ok, trying without user" >> $OUT
       
-      mysql -h "127.0.0.1" -e "select 1" 1>> $OUT 2>> $OUT
+      mysql -h "127.0.0.1" -e "select 1"
+      # 1>> $OUT 2>> $OUT
       if [ $? ne 0 ]; then
-        echo "select not ok"
+#        echo "select not ok"
         exit -1
-      else
-        echo "select ok for no user"
-        exit 0
       fi
-    else
-      echo "select ok for root without password"
-      exit 0
     fi
-  else
-    echo "select ok" >> $OUT
-    exit 0
   fi
-#else
-#  echo "cluster not started yet" >> $OUT
-#  exit -2
-#fi
+  exit 0
+  
