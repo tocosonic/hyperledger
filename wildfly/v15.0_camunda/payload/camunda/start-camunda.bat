8d4bd14b3ef3a5d1b444ed700be019eca6389bd4
Michael Hinkel <michael.hinkel-extern@deutschebahn.com>
2019-05-15 21:34:31 +0200

    added dockerfile for wildfly+camunda; added mysql driver to wildfly

---
+++ wildfly/v15.0_camunda/payload/camunda/start-camunda.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set "JBOSS_HOME=%CD%\server\wildfly-16.0.0.Final"
+
+echo "starting camunda BPM platform 7.11.0-alpha4 on Wildfly Application Server 16.0.0.Final"
+
+cd server\wildfly-16.0.0.Final\bin\
+start standalone.bat
+
+ping -n 5 localhost > NULL
+start http://localhost:8080/camunda-welcome/index.html
+ 
